422caddd38d222b8cf4d7c3ed6671fd729fa832f
George Hack <george.hack.2@gmail.com>
2023-11-16 15:22:36 -0500

    Declare the 2 dimensional array

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -5,20 +5,18 @@ from levelup.direction import Direction
 class Map ():
 
     starting_position = Position(0,0)
-    positions = []
+    positions = [[0 for x in range(10)] for y in range(10)]
     size: Tuple[int, int] = (10, 10)
 
     # Exists for easy testing
     num_positions = size[0]*size[1]
 
     def __init__(self):
-        # TODO: implement method here and remove the print statement below
-       
         for x_val in range(10):
              for y_val in range(10):
-                self.positions.append(Position(x_val,y_val))
-        # print("map constructor method not yet implemented")
-
+                print("x_val", x_val, "y_val", y_val)
+                self.positions[x_val][y_val] = Position(x_val, y_val)
+        
     def is_position_valid(self, position :Position) -> bool:
         # TODO: implement method here and remove the print statement below
         print("is_position_valid method not yet implemented")
